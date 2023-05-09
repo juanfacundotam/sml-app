2b22fcc1b6b22568afe7e0e08f6c20a0098e3e3e
mario19martinez <mariomartinez678abc@gmail.com>
2023-05-09 14:55:54 -0500

    Contract

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+//import '@tremor/react/dist/esm/tremor.css'
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <div className=" bg-red-600">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.html", "./src/**/**/**/*.jsx"],
+  content: ["./*.html", "./src/**/**/**/*.jsx",
+
+  // Path to the Tremor module
+"./node_modules/@tremor//*.{js,ts,jsx,tsx}",
+  // "./node_modules/@tremor/**/*.{js, jsx}"
+],
   theme: {
     extend: {},
   },
