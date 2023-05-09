803e079593b9c034bcc8994771b246124ff57e9b
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2023-05-09 12:33:48 -0300

    Cambios

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -3,10 +3,8 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 function Login() {
   const{loginWithRedirect} = useAuth0()
-
   return (
     <div>
-
       <button onClick={()=> loginWithRedirect()}>LOGIN</button>
     </div>
   )
