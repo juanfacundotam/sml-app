0f5babd4bb6d6b0a6c48d6c354e5a8c81bb7916d
juanfacundotam <facutam@gmail.com>
2023-05-19 16:02:28 -0300

    funcionalidad nueva vendedoresDashboard
    Se saco los datos del modal. se esta intentando hacer un contador, para estableces el rechazo por 3 intentos de contacto fallido

--- api/src/controllers/Lead/updateLeadVendedorById.js
+++ api/src/controllers/Lead/updateLeadVendedorById.js
@@ -3,40 +3,41 @@ const Vendedor = require("../../models/Vendedor");
 
 const updateLeadVendedorById = async (id, updatedData) => {
   // console.log(updatedData.dataLead);
-    const lead = await Lead.findByIdAndUpdate(id, updatedData.dataLead, {
-      new: true,
-    });
+  const leadCountCheck = await Lead.findById(id);
+  console.log(leadCountCheck.noresponde_count)
 
-    const valor = updatedData.dataVendedor;
-    console.log(updatedData.dataLead.status);
+  const leadUpdate = await Lead.findByIdAndUpdate(id, updatedData.dataLead, {
+    new: true,
+  });
 
-    let property = "";
-    if (updatedData.dataLead.status === "Contratado") {
-      property = "leads_contacted";
-    } else if (updatedData.dataLead.status === "Rechazado") {
-      property = "declined_leads";
-    } else if (updatedData.dataLead.status === "No responde") {
-      property = "unanswered_leads";
-    } else {
-      property = "hired_leads";
-    }
+  const valor = updatedData.dataVendedor;
+  console.log(updatedData.dataLead.status);
 
-    const vendedor = await Vendedor.findOneAndUpdate(
-      { email: updatedData.dataLead.vendedor },
-      { $push: { [property]: { $each: [valor] } } },
-      { new: true }
-    );
+  let property = "";
+  if (updatedData.dataLead.status === "Contratado") {
+    property = "leads_contacted";
+  } else if (updatedData.dataLead.status === "Rechazado") {
+    property = "declined_leads";
+  } else if (updatedData.dataLead.status === "No responde") {
+    property = "unanswered_leads";
+  } else {
+    property = "hired_leads";
+  }
 
-    const data = {
-      lead,
-      vendedor,
-    };
+  const vendedor = await Vendedor.findOneAndUpdate(
+    { email: updatedData.dataLead.vendedor },
+    { $push: { [property]: { $each: [valor] } } },
+    { new: true }
+  );
 
-    // Imprimir la publicación completa
-    return data;
-    // return lead;
+  const data = {
+    leadUpdate,
+    vendedor,
+  };
 
+  // Imprimir la publicación completa
+  return data;
+  // return lead;
 };
 
 module.exports = updateLeadVendedorById;
-
--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -12,6 +12,7 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
+  height: 440,
   bgcolor: "#39394B",
   border: "none",
   boxShadow: 24,
@@ -41,6 +42,7 @@ function ChildModal({
   const handleUpdate = () => {
     const dataVendedor = item.name;
 
+    
     const dataLead = {
       status: statusObj.status,
       status_op: statusObj.status_op,
@@ -278,12 +280,20 @@ export default function NestedModal({
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 550, borderRadius: 5 }}>
-          <h2 id="parent-modal-title" className="text-center text-white mb-6">
+        <Box sx={{ ...style, width: 550, borderRadius: 5}}>
+            <div className="w-full flex justify-center items-center mt-3 mb-10">
+          <h2 id="parent-modal-title" className="text-center text-white">
             {item.name}
           </h2>
-          <div className="flex flex-col">
-            <div className="mt-2">
+          <div className="flex flex-col absolute right-4 top-4">
+              <div className="bg-[#8d8b0c] text-[#e8e8e9] w-[40px] rounded-md h-9 text-[35px] drop-shadow-xl hover:bg-[#c94219] ">
+                <IncidenceModal
+                  setOpen={setOpen}
+                  SendIncidenceAlert={SendIncidenceAlert}
+                />
+              </div>
+            </div>
+            {/* <div className="mt-2">
               <label
                 htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -301,8 +311,8 @@ export default function NestedModal({
                 required
                 disabled
               />
-            </div>
-            <div className="mt-5">
+            </div> */}
+            {/* <div className="mt-5">
               <label
                 htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -319,8 +329,8 @@ export default function NestedModal({
                 required
                 disabled
               />
-            </div>
-            <div className="mt-5">
+            </div> */}
+            {/* <div className="mt-5">
               <label
                 htmlFor="last_name"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -337,18 +347,10 @@ export default function NestedModal({
                 required
                 disabled
               />
-              <div className=" w-full flex justify-end items-center mt-3">
-                <div className=" bg-[#8d8b0c] text-[#e8e8e9] w-[40px] rounded h-9 text-[35px] drop-shadow-xl hover:bg-[#c94219] ">
-                  <IncidenceModal
-                    setOpen={setOpen}
-                    SendIncidenceAlert={SendIncidenceAlert}
-                  />
-                </div>
-              </div>
-            </div>
+            </div> */}
           </div>
 
-          <div className=" flex items-center justify-between flex-col mb-10">
+          <div className="h-52 flex items-center justify-start flex-col mb-10">
             <div className="">
               <label
                 htmlFor="countries"
@@ -390,6 +392,7 @@ export default function NestedModal({
                   <option value="Sin dinero">Sin Dinero</option>
                   <option value="Sin interes">Sin Interes</option>
                   <option value="Otro servicio">Otro Servicio</option>
+                  {/* <option value="Otro servicio">Sin respuesta - 3 Llamados</option> */}
                   {/* <option value="DE">Germany</option> */}
                 </select>
               </div>
