87f195f5ae7cafa94466e4ca2ae186df6883d5bc
juanfacundotam <facutam@gmail.com>
2023-05-20 12:51:12 -0300

    commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store"
 import axios from "axios";
 
-
+//comnetarioaosiorisoirosi
 // axios.defaults.baseURL = "https://henry-food-api-production.up.railway.app/";
 // axios.defaults.baseURL = "http://localhost:3001";
 
