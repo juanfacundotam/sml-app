3f628a46ee186ebaab3c1f4d975be150595864ea
juanfacundotam <facutam@gmail.com>
2023-05-23 10:39:10 -0300

    fixed ruta

--- api/src/controllers/Vendedor/getVendedorByEmail.js
+++ api/src/controllers/Vendedor/getVendedorByEmail.js
@@ -4,7 +4,6 @@ const getVendedorByEmail = async (email) => {
 
 	// const regex = new RegExp(email, 'i'); // 'i' indica que la búsqueda es insensible a mayúsculas y minúsculas
 	const vendedor = await Vendedor.findOne({ email: email });
-    console.log(vendedor)
 	return vendedor;
 };
 
