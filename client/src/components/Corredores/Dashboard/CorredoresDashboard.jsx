d4a1f5cc1c6333588bb88c8a7f8eebd5ea963197
Jonavoe <94918075+Jonavoe@users.noreply.github.com>
2023-05-24 17:25:31 -0300

    Update CorredoresDashboard.jsx

--- client/src/components/Corredores/Dashboard/CorredoresDashboard.jsx
+++ client/src/components/Corredores/Dashboard/CorredoresDashboard.jsx
@@ -215,7 +215,7 @@ const CorredoresDashboard = () => {
         if (client[i].level !== "-") {
           if (client[i].instagram.trim() !== "" && client[i].level === "0") {
             SendLeadsErrorInsta0(client[i].name);
-          } else if (client[i].email !== "" && client[i].email === "-") {
+          } else if ((client[i].email === "" || client[i].email === "-") && (client[i].level !== "0" && client[i].level !== "incidencia")) {
             SendLeadsErrorEmail(client[i].name);
           } else if (
             client[i].instagram.trim() === "" &&
