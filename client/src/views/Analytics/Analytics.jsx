af992b2861d574764c127593e3c4f6e289c0bd44
Nikitoo17 <nicolasfadel17@gmail.com>
2023-05-24 18:04:40 -0300

    clean code

--- client/src/views/Analytics/Analytics.jsx
+++ client/src/views/Analytics/Analytics.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Nav from "../../components/Nav/Nav";
-import AnalyticsNumerics from "../../components/C-Level/Analytics/AnalyticsNumerics/AnalyticsNumerics";
-import Reports from "../../components/C-Level/Analytics/Reports/Reports";
-import Goals from "../../components/C-Level/Analytics/Goals/Goals";
-import BestSellers from "../../components/C-Level/Analytics/BestSellers/BestSellers";
-import { Sales } from "../../components/C-Level/Analytics/Sales/Sales";
-
 import Analytic from "../../components/C-Level/Analytics/Analytic";
 
 function Analytics() {
   return (
-    
     <>
-    <Nav/>
-       <Analytic /> 
-     {/* <AnalyticLeader />*/}
+      <Nav />
+      <Analytic />
     </>
   );
 }
