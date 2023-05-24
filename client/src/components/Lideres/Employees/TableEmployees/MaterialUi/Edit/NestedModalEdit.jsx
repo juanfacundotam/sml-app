9d4311ba679ad42ec71e483802d6c9a638d67518
Nikitoo17 <nicolasfadel17@gmail.com>
2023-05-24 17:55:20 -0300

    clean code

--- client/src/components/Lideres/Employees/TableEmployees/MaterialUi/Edit/NestedModalEdit.jsx
+++ client/src/components/Lideres/Employees/TableEmployees/MaterialUi/Edit/NestedModalEdit.jsx
@@ -45,12 +45,9 @@ function ChildModalDelete({
 
   const handleCreate = async () => {
     try {
-      const response = await axios.put(
-        `/${itemRol}/${itemId}`,
-        {
-          deleted: true,
-        }
-      );
+      const response = await axios.put(`/${itemRol}/${itemId}`, {
+        deleted: true,
+      });
 
       BannedEmployees(inputName);
       onModalClose();
@@ -125,15 +122,12 @@ function ChildModal({
     }
 
     try {
-      const response = await axios.put(
-        `/${itemRol}/${itemId}`,
-        {
-          name: inputName,
-          email: inputEmail,
-          rol: selectEmployees,
-          contactNumber: inputPhone,
-        }
-      );
+      const response = await axios.put(`/${itemRol}/${itemId}`, {
+        name: inputName,
+        email: inputEmail,
+        rol: selectEmployees,
+        contactNumber: inputPhone,
+      });
       EditEmployees(inputName);
       onModalClose();
       console.log(response.data);
@@ -231,10 +225,6 @@ export default function NestedModalEdit({
                 inputPhone={inputPhone}
                 setInputPhone={setInputPhone}
               />
-              {/* <BasicSelect
-                employees={selectEmployees}
-                setEmployees={setSelectEmployees}
-              /> */}
             </div>
           </div>
           <div className="flex gap-3 justify-center items-center">
