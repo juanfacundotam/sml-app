1b55661068e7468dd564608ea9c78d8688a09f0d
juanfacundotam <facutam@gmail.com>
2023-05-26 21:57:39 -0300

    Revert "Revert "Squashed commit of the following:""
    This reverts commit 56106acc0667424241caeee0064b42d83a3f79bd.

--- api/src/controllers/Lead/getLead10Unchecked.js
+++ api/src/controllers/Lead/getLead10Unchecked.js
@@ -1,13 +1,81 @@
 const Lead = require("../../models/Lead");
 
-const getLead10Unchecked = async (email, limit) => {
+
+const getLead10Unchecked = async (email) => {
+ 
+
   const leadUnchecked = await Lead.find({
     corredor: email,
     checked: false,
+    view: false,
   })
-    .limit(limit)
+    .limit(1)
     .exec();
-  return leadUnchecked;
+
+  let count = 0;
+  count = 10 - leadUnchecked.length;
+
+  const leadRest = await Lead.find({
+    checked: false,
+    view: false,
+    corredor: "",
+  })
+    .limit(count)
+    .exec();
+
+  if (leadRest.length > 0) {
+    leadRest.forEach((element) => {
+      element.corredor = email;
+      element.save();
+    });
+  }
+
+  return [...leadUnchecked, ...leadRest];
+
 };
 
 module.exports = getLead10Unchecked;
+
+// const emailProp = email.email;
+// const leadChequedInactive = await Lead.find({
+//   checked: true,
+//   vendedor: emailProp,
+//   status: "Sin contactar",
+//   level: { $nin: ["incidencia", "0", "", "-"] },
+// })
+//   .limit(5)
+//   .exec();
+
+// const leadChequedInactiveNoResponde = await Lead.find({
+//   checked: true,
+//   vendedor: emailProp,
+//   $or: [{ status: "No responde" }],
+//   level: { $nin: ["incidencia", "0", "", "-"] },
+// });
+
+// let count = 0;
+
+// count = 5 - leadChequedInactive.length;
+// console.log(count);
+
+// const leadRest = await Lead.find({
+//   checked: true,
+//   vendedor: "",
+//   status: "Sin contactar",
+//   level: { $nin: ["incidencia", "0", "", "-"] },
+// })
+//   .limit(count)
+//   .exec();
+
+// if (leadRest.length > 0) {
+//   leadRest.forEach((element) => {
+//     element.vendedor = emailProp;
+//     element.save();
+//   });
+// }
+
+// return [
+//   ...leadChequedInactive,
+//   ...leadRest,
+//   ...leadChequedInactiveNoResponde,
+// ];
--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -111,6 +111,7 @@ export const getLeadCheckedInactive5 = (email) => {
   return async (dispatch) => {
     const response = await axios.get(`/lead/checkedinactive5?email=${email}`);
     const LeadCheckedInactive5 = response.data;
+    console.log(LeadCheckedInactive5)
     dispatch({
       type: GET_LEAD_CHEQUED_INACTIVE_5,
       payload: LeadCheckedInactive5,
