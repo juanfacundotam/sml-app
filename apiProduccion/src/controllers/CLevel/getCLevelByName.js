e1f4f0fa558c3d4753ee349184351b7b6ff6d563
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-28 21:19:32 -0300

    se elimina apiProduccion

--- apiProduccion/src/controllers/CLevel/getCLevelByName.js
+++
@@ -1,9 +0,0 @@
-const CLevel = require("../../models/CLevel");
-
-const getCLevelByName = async (Name) => {
-  const regex = new RegExp(Name, "i");
-  const cLevels = await CLevel.find({ Name: { $regex: regex } });
-  return cLevels;
-};
-
-module.exports = getCLevelByName;
--- apiProduccion/src/controllers/Vendedor/getVendedorByEmail.js
+++
@@ -1,9 +0,0 @@
-const Vendedor = require("../../models/Vendedor");
-
-const getVendedorByEmail = async (email) => {
-  const vendedor = await Vendedor.findOne({ email: email });
-
-  return vendedor;
-};
-
-module.exports = getVendedorByEmail;
