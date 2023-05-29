fb7c57127312b5b45f0dd1912884ba01e24d4533
Mario Martinez <mariomartinez678abc@gmail.com>
2023-05-29 11:19:34 -0500

    eliminando inncecesarios

--- api/src/routes/ProfileRouter.js
+++
@@ -1,9 +0,0 @@
-
-const {Router} = require("express")
-const {updateProfileHandler} = require("../Handlers/profileHandler")
-
-const ProfileRouter = Router();
-
-ProfileRouter.put("/profile", updateProfileHandler);
-
-module.exports = ProfileRouter;
--- api/src/routes/router.js
+++ api/src/routes/router.js
@@ -6,7 +6,6 @@ const LeadRouter = require("./LeadRouter");
 const LeaderRouter = require("./LeaderRouter");
 const VendedorRouter = require("./VendedorRouter");
 const EmployeesRouter = require("./EmployeesRouter");
-const ProfileRouter = require("./ProfileRouter")
 
 router.use("/employees", EmployeesRouter);
 router.use("/clevel", CLevelRouter);
@@ -14,6 +13,5 @@ router.use("/corredor", CorredorRouter);
 router.use("/lead", LeadRouter);
 router.use("/leader", LeaderRouter);
 router.use("/vendedor", VendedorRouter);
-router.use("/profile", ProfileRouter);
 
 module.exports = router;
