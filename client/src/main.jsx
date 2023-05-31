899046bec6144ac870003a740fe3d0f1aaf6a5fa
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-31 09:03:29 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,8 +9,8 @@ import store from "./redux/store";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:3001/api";
-axios.defaults.baseURL = "https://sml-app-api.onrender.com/api";
-// axios.defaults.baseURL = "https://sml-app.com/api";
+// axios.defaults.baseURL = "https://sml-app-api.onrender.com/api";
+axios.defaults.baseURL = "https://sml-app.com/api";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
