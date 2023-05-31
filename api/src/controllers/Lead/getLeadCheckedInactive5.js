bfbb61f3d022ccacb8e33b35fab8e284ca7fbb59
juanfacundotam <facutam@gmail.com>
2023-05-31 10:42:19 -0300

    optimizacion funcion vendedores
    Se optimizo la funcion que extrae los datos de la base

--- api/src/controllers/Lead/getLeadCheckedInactive5.js
+++ api/src/controllers/Lead/getLeadCheckedInactive5.js
@@ -20,32 +20,33 @@ const getLeadCheckedInactive5 = async (email) => {
 
   let count = 0;
   count = 5 - leadChequedInactive.length;
-console.log(count)
+  console.log(count);
   let leadRest = [];
-  if (count > 0 && count <= 5) {
-    leadRest = await Lead.find({
-      checked: true,
-      vendedor: "",
-      status: "Sin contactar",
-      level: { $nin: ["incidencia", "0", "", "-"] },
-    })
-      .limit(count)
-      .exec();
+  if (count) {
+    if (count > 0 && count <= 5) {
+      leadRest = await Lead.find({
+        checked: true,
+        vendedor: "",
+        status: "Sin contactar",
+        level: { $nin: ["incidencia", "0", "", "-"] },
+      })
+        .limit(count)
+        .exec();
 
-
-    // if (leadRest.length > 0) {
-    //   await Promise.all(
-    //     leadRest.map(async (element) => {
-    //       element.vendedor = email;
-    //       await element.save();
-    //     })
-    //   );
-    // }
-    if (leadRest.length > 0) {
-      leadRest.forEach((element) => {
-        element.vendedor = email;
-        element.save();
-      });
+      if (leadRest.length > 0) {
+        await Promise.all(
+          leadRest.map(async (element) => {
+            element.vendedor = email;
+            await element.save();
+          })
+        );
+      }
+      // if (leadRest.length > 0) {
+      //   leadRest.forEach((element) => {
+      //     element.vendedor = email;
+      //     element.save();
+      //   });
+      // }
     }
   }
 
@@ -58,42 +59,3 @@ console.log(count)
 
 module.exports = getLeadCheckedInactive5;
 
-// const Lead = require("../../models/Lead");
-
-// const getCorredorLead10 = async (email) => {
-//   const leadUnchecked = await Lead.find({
-//     corredor: email,
-//     checked: false,
-//   })
-//     .limit(10)
-//     .exec();
-
-//   let count = 0;
-//   count = 10 - leadUnchecked.length;
-//   console.log(count);
-
-//   let leadRest = [];
-
-//   if (count > 0 && count <= 10) {
-//     const leadRest = await Lead.find({
-//       checked: false,
-//       corredor: "",
-//     })
-//       .limit(count)
-//       .exec();
-
-//     if (leadRest.length > 0) {
-//       await Promise.all(
-//         leadRest.map(async (element) => {
-//           element.corredor = email;
-//           await element.save();
-//         })
-//       );
-//     }
-//   }
-
-//   console.log(...leadUnchecked, ...leadRest);
-//   return [...leadUnchecked, ...leadRest];
-// };
-
-// module.exports = getCorredorLead10;
