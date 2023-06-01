bf3954633297496b08f1e539d97c59ccb8c225b6
Jonavoe <voeffray.jonathan@gmail.com>
2023-05-31 21:49:24 -0300

    Update main.jsx
    Co-Authored-By: Nikitoo17 <114702304+Nikitoo17@users.noreply.github.com>
    Co-Authored-By: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,9 +8,9 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+// axios.defaults.baseURL = "http://localhost:3001/api";
 // axios.defaults.baseURL = "https://sml-app-api.onrender.com/api";
-// axios.defaults.baseURL = "https://sml-app.com/api";
+axios.defaults.baseURL = "https://sml-app.com/api";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
