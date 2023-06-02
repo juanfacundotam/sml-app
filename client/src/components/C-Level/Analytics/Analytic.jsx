43af55be6c99b756179168979657a6934544da6a
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-02 15:54:13 -0300

    Update Analytic.jsx

--- client/src/components/C-Level/Analytics/Analytic.jsx
+++ client/src/components/C-Level/Analytics/Analytic.jsx
@@ -4,10 +4,10 @@ function Analytic() {
   return (
     <div className="flex flex-col h-screen w-screen">
       <iframe
-        title="dashboard SML"
+        title="Report Section"
         width="100%"
         height="100%"
-        src="https://app.powerbi.com/view?r=eyJrIjoiY2NlZGM5ZjItODdjYS00YmM1LTgzODMtMDAzNTYyYjcxZjVkIiwidCI6ImZhMTYyNmJlLTUxMDMtNGM5MC1iYzJmLTY2NzAxMWIzMzAwYyJ9&pageName=ReportSection"
+        src="https://app.powerbi.com/view?r=eyJrIjoiZDgwODliOWMtNTQyNi00NmUwLWEyNWUtZDk2ODM5ZDBkNjk3IiwidCI6ImZhMTYyNmJlLTUxMDMtNGM5MC1iYzJmLTY2NzAxMWIzMzAwYyJ9"
         frameborder="0"
         allowFullScreen="true"
       ></iframe>
