b907c76690b1b3252b3b5ff0ebd6242c6c5aee6c
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-02 18:44:42 -0300

    Update Analytic.jsx

--- client/src/components/C-Level/Analytics/Analytic.jsx
+++ client/src/components/C-Level/Analytics/Analytic.jsx
@@ -3,22 +3,22 @@ import React from "react";
 function Analytic() {
   return (
     <div className="flex flex-col h-screen w-screen">
-      <iframe
+      {/* <iframe
         title="dashboard SML"
         width="100%"
         height="100%"
         src="https://app.powerbi.com/view?r=eyJrIjoiY2NlZGM5ZjItODdjYS00YmM1LTgzODMtMDAzNTYyYjcxZjVkIiwidCI6ImZhMTYyNmJlLTUxMDMtNGM5MC1iYzJmLTY2NzAxMWIzMzAwYyJ9&pageName=ReportSection"
         frameborder="0"
         allowFullScreen="true"
-      ></iframe>
-      {/* <iframe
+      ></iframe> */}
+      <iframe
         title="Report Section"
         width="100%"
         height="100%"
         src="https://app.powerbi.com/view?r=eyJrIjoiZDgwODliOWMtNTQyNi00NmUwLWEyNWUtZDk2ODM5ZDBkNjk3IiwidCI6ImZhMTYyNmJlLTUxMDMtNGM5MC1iYzJmLTY2NzAxMWIzMzAwYyJ9"
         frameborder="0"
         allowFullScreen="true"
-      ></iframe> */}
+      ></iframe>
     </div>
   );
 }
